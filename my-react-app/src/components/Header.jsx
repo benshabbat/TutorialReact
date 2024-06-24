037de4bd2ad652af81af5f37fbf3cd37292f3a0b
benshabbat <benshabbat27@gmail.com>
2024-06-24 21:23:21 +0300

    done header

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Welome to exam how to create components</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Food</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
