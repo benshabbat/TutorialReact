3a3ff63570cddcc1ed8b74a4fefffa045f376e69
benshabbat <benshabbat27@gmail.com>
2024-06-24 21:32:30 +0300

    done app

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Header from "./components/Header"
+import Food from "./components/Food"
+import Footer from "./components/Footer"
 function App() {
 
 
   return (
     <>
-
+      <Header/>
+      <Food/>
+      <Footer/>
     </>
   )
 }
