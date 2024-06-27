d388108c248a582a3ffb191bc061fbf12661c1e4
benshabbat <benshabbat27@gmail.com>
2024-06-27 17:58:35 +0300

    add img

--- my-react-app/src/components/Card.jsx
+++ my-react-app/src/components/Card.jsx
@@ -1,8 +1,8 @@
-
+import img_my_profile from "../images/my_profile.jpg";
 const Card = () => {
   return (
     <div>
-        <img></img>
+        <img src={img_my_profile} alt="my profile"></img>
         <h2>David Chen Benshabbat</h2>
         <p>i teaching how to make card component</p>
     </div>
