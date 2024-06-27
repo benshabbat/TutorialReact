da17b130d31571edf218430d8bada642a0efee0d
benshabbat <benshabbat27@gmail.com>
2024-06-27 19:07:50 +0300

    done card img css

--- my-react-app/src/components/Card.jsx
+++ my-react-app/src/components/Card.jsx
@@ -2,7 +2,7 @@ import img_my_profile from "../images/my_profile.jpg";
 const Card = () => {
   return (
     <div className="card">
-        <img src={img_my_profile} alt="my profile" className="img-profile"></img>
+        <img src={img_my_profile} alt="my profile" className="card-img"></img>
         <h2>David Chen Benshabbat</h2>
         <p>i teaching how to make card component</p>
     </div>
