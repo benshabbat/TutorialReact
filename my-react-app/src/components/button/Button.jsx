c910461f37f35eb43dfbd67e1ceea2ee6d01c41b
benshabbat <benshabbat27@gmail.com>
2024-06-29 23:36:33 +0300

    inline style

--- my-react-app/src/components/button/Button.jsx
+++ my-react-app/src/components/button/Button.jsx
@@ -1,9 +1,18 @@
 import style from "./button.module.css";
 
 const Button = () => {
+  const buttonStyle = {
+    backgroundColor: "blueviolet",
+    borderRadius: "20%",
+    border: "1px solid gray",
+    color: "wheat",
+    padding: "5px 10px",
+    cursor: "pointer",
+  };
   return (
-    <button className={style.button}>Click me</button>
-  )
-}
+    <button style={buttonStyle}>Click me</button>
+    // <button className={style.button}>Click me</button>
+  );
+};
 
-export default Button
+export default Button;
