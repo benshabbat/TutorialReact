df516179f752ef5da9364035ac4f67b3fbc09167
benshabbat <benshabbat27@gmail.com>
2024-07-01 22:17:05 +0300

    made variables for ternary

--- my-react-app/src/components/UserGreeting.jsx
+++ my-react-app/src/components/UserGreeting.jsx
@@ -1,23 +1,17 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 const UserGreeting = (props) => {
-  return (
-    <div>
-      {props.isLoggedIn ? (
-        <h1>Welcome {props.username} </h1>
-      ) : (
-        <h1>Please log in</h1>
-      )}
-    </div>
-  );
+  const welcome = <h1>Welcome {props.username} </h1>;
+  const login = <h1>Please log in</h1>;
+  return <div>{props.isLoggedIn ? welcome : login}</div>;
 };
-UserGreeting.proptypes={
-    isLoggedIn: PropTypes.bool,
-    username: PropTypes.string,
-}
-UserGreeting.defaultProps={
-    isLoggedIn:false,
-    username: "Guest"
-}
+UserGreeting.proptypes = {
+  isLoggedIn: PropTypes.bool,
+  username: PropTypes.string,
+};
+UserGreeting.defaultProps = {
+  isLoggedIn: false,
+  username: "Guest",
+};
 
 export default UserGreeting;
