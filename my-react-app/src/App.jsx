1e3ea00b1a993826a62120cd4b56e5922b03f74b
benshabbat <benshabbat27@gmail.com>
2024-07-03 14:01:26 +0300

    fix fruitsobjects

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,11 +5,13 @@ import Card from "./components/Card";
 import Button from "./components/button/Button";
 import UserGreeting from "./components/UserGreeting";
 import Fruits from "./components/Fruits";
+import FruitsObjects from "./components/FruitsObjects";
 
 function App() {
   return (
     <>
-      <Fruits />
+      <FruitsObjects />
+      {/* <Fruits /> */}
 
       {/* <Button /> */}
       {/* <UserGreeting isLoggedIn={true} username="Benshabbat"/> */}
--- my-react-app/src/components/FruitsObjects.jsx
+++ my-react-app/src/components/FruitsObjects.jsx
@@ -1,17 +1,18 @@
 const FruitsObjects = () => {
+  const fruits = [
+    { id: 1, name: "apple", calories: 52.1, gram: 100 },
+    { id: 2, name: "watermelon", calories: 30.4, gram: 100 },
+    { id: 3, name: "banana", calories: 88.7, gram: 100 },
+    { id: 4, name: "orange", calories: 47.1, gram: 100 },
+  ];
+  const listFruits = fruits.map((fruit) => {
+    return (
+      <li key={fruit.id}>
+        Name:{fruit.name} Calories:{fruit.calories}
+      </li>
+    );
+  });
+  return <ul>{listFruits}</ul>;
+};
 
-    const fruits = [
-        {id:1,name:'apple',calories:52.1,gram:100},
-        {id:1,name:'watermelon',calories:30.4,gram:100},
-        {id:1,name:'banana',calories:88.7,gram:100},
-        {id:1,name:'orange',calories:47.1,gram:100},
-    ]
-    const listFruits = fruits.map(fruit=>{
-        <li key={fruit.id}>Name:{fruit.name} Calories:{fruit.calories}</li>
-    })
-  return (
-    <div>{listFruits}</div>
-  )
-}
-
-export default FruitsObjects
+export default FruitsObjects;
