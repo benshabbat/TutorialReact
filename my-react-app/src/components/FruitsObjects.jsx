502ae1825f116ec6eb8447a37318c26f6d0686b0
benshabbat <benshabbat27@gmail.com>
2024-07-03 13:50:53 +0300

    made list objects of fruits

---
+++ my-react-app/src/components/FruitsObjects.jsx
@@ -0,0 +1,15 @@
+const FruitsObjects = () => {
+
+    const fruits = [
+        {id:1,name:'apple',calories:52.1,gram:100},
+        {id:1,name:'watermelon',calories:30.4,gram:100},
+        {id:1,name:'banana',calories:88.7,gram:100},
+        {id:1,name:'orange',calories:47.1,gram:100},
+    ]
+
+  return (
+    <div>FruitsObjects</div>
+  )
+}
+
+export default FruitsObjects
