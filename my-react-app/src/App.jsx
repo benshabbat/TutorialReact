e46d2c6027b65665cc0b932a30a3192250bff460
benshabbat <benshabbat27@gmail.com>
2024-07-03 12:39:47 +0300

    exam for lists

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,12 +5,13 @@ import Card from "./components/Card";
 import Button from "./components/button/Button";
 import UserGreeting from "./components/UserGreeting";
 import List from "./components/List";
+
 function App() {
   return (
     <>
+    <List/>
       {/* <Button /> */}
       {/* <UserGreeting isLoggedIn={true} username="Benshabbat"/> */}
-      <List/>
       {/* <Card /> */}
       {/* <Header/>
       <Food/>
